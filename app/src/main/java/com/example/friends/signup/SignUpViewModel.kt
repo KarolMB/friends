0dd9a586a58f2de2685eb818bb7086fc4bc733ff
karol <email@com>
2023-10-18 21:55:48 +0200

    Separate state assigning

--- app/src/main/java/com/example/friends/signup/SignUpViewModel.kt
+++ app/src/main/java/com/example/friends/signup/SignUpViewModel.kt
@@ -16,12 +16,17 @@ class SignUpViewModel {
     ) {
         val EMAIL_REGEX =
             """[a-zA-Z0-9+._%\-]{1,64}@[a-zA-Z0-9][a-zA-Z0-9\-]{1,64}(\.[a-zA-Z0-9][a-zA-Z0-9\-]{1,25})"""
+        val PASSWORD_REGEX = """^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*+=\-]).{8,}$"""
+
         val emailPattern = Pattern.compile(EMAIL_REGEX)
-        if (!emailPattern.matcher(email).matches()) {
-            _mutableSignUpState.value = SignUpState.BadEmail
-        } else if (password == "") {
-            _mutableSignUpState.value = SignUpState.BadPassword
-        }
+        val passwordPattern = Pattern.compile(PASSWORD_REGEX)
+
+        val state = if (!emailPattern.matcher(email).matches()) {
+            SignUpState.BadEmail
+        } else if (!passwordPattern.matcher(password).matches()) {
+            SignUpState.BadPassword
+        } else TODO()
+        _mutableSignUpState.value = state
 
     }
 
