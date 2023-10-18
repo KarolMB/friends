5cfa26553307e02f553b8c6c58ff06952a8efff5
karol <email@com>
2023-10-18 22:06:01 +0200

    Extract the validation into separate class

--- app/src/main/java/com/example/friends/signup/SignUpViewModel.kt
+++ app/src/main/java/com/example/friends/signup/SignUpViewModel.kt
@@ -14,33 +14,36 @@ class SignUpViewModel {
         password: String,
         about: String
     ) {
-        when (credentialsValidationResult(email, password)) {
+        when (RegexCredentialsValidator().credentialsValidationResult(email, password)) {
             is CredentialsValidationResult.InvalidEmail -> {
                 _mutableSignUpState.value = SignUpState.BadEmail
             }
+
             is CredentialsValidationResult.InvalidPassword -> {
                 _mutableSignUpState.value = SignUpState.BadPassword
             }
         }
     }
 
-    private fun credentialsValidationResult(
-        email: String,
-        password: String
-    ): CredentialsValidationResult {
-        val EMAIL_REGEX =
-            """[a-zA-Z0-9+._%\-]{1,64}@[a-zA-Z0-9][a-zA-Z0-9\-]{1,64}(\.[a-zA-Z0-9][a-zA-Z0-9\-]{1,25})"""
-        val PASSWORD_REGEX = """^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*+=\-]).{8,}$"""
+    class RegexCredentialsValidator {
+        fun credentialsValidationResult(
+            email: String,
+            password: String
+        ): CredentialsValidationResult {
+            val EMAIL_REGEX =
+                """[a-zA-Z0-9+._%\-]{1,64}@[a-zA-Z0-9][a-zA-Z0-9\-]{1,64}(\.[a-zA-Z0-9][a-zA-Z0-9\-]{1,25})"""
+            val PASSWORD_REGEX = """^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*+=\-]).{8,}$"""
 
-        val emailPattern = Pattern.compile(EMAIL_REGEX)
-        val passwordPattern = Pattern.compile(PASSWORD_REGEX)
+            val emailPattern = Pattern.compile(EMAIL_REGEX)
+            val passwordPattern = Pattern.compile(PASSWORD_REGEX)
 
-        val result = if (!emailPattern.matcher(email).matches()) {
-            CredentialsValidationResult.InvalidEmail
-        } else if (!passwordPattern.matcher(password).matches()) {
-            CredentialsValidationResult.InvalidPassword
-        } else TODO()
-        return result
+            val result = if (!emailPattern.matcher(email).matches()) {
+                CredentialsValidationResult.InvalidEmail
+            } else if (!passwordPattern.matcher(password).matches()) {
+                CredentialsValidationResult.InvalidPassword
+            } else TODO()
+            return result
+        }
     }
 
     sealed class CredentialsValidationResult {
