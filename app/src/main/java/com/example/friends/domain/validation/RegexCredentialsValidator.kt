4dd5a7629680e58b15672f1f368d744e92e5507f
karol <email@com>
2023-10-19 10:17:56 +0200

    Extract validator into separate package

---
+++ app/src/main/java/com/example/friends/domain/validation/RegexCredentialsValidator.kt
@@ -0,0 +1,24 @@
+package com.example.friends.domain.validation
+
+import java.util.regex.Pattern
+
+class RegexCredentialsValidator {
+    fun credentialsValidationResult(
+        email: String,
+        password: String
+    ): CredentialsValidationResult {
+        val EMAIL_REGEX =
+            """[a-zA-Z0-9+._%\-]{1,64}@[a-zA-Z0-9][a-zA-Z0-9\-]{1,64}(\.[a-zA-Z0-9][a-zA-Z0-9\-]{1,25})"""
+        val PASSWORD_REGEX = """^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*+=\-]).{8,}$"""
+
+        val emailPattern = Pattern.compile(EMAIL_REGEX)
+        val passwordPattern = Pattern.compile(PASSWORD_REGEX)
+
+        val result = if (!emailPattern.matcher(email).matches()) {
+            CredentialsValidationResult.InvalidEmail
+        } else if (!passwordPattern.matcher(password).matches()) {
+            CredentialsValidationResult.InvalidPassword
+        } else TODO()
+        return result
+    }
+}
--- app/src/main/java/com/example/friends/signup/SignUpViewModel.kt
+++ app/src/main/java/com/example/friends/signup/SignUpViewModel.kt
@@ -2,10 +2,13 @@ package com.example.friends.signup
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.example.friends.domain.validation.CredentialsValidationResult
+import com.example.friends.domain.validation.RegexCredentialsValidator
 import com.example.friends.signup.state.SignUpState
-import java.util.regex.Pattern
 
-class SignUpViewModel {
+class SignUpViewModel(
+    private val credentialsValidator: RegexCredentialsValidator
+) {
 
     private val _mutableSignUpState = MutableLiveData<SignUpState>()
     val signUpState: LiveData<SignUpState> = _mutableSignUpState
@@ -14,7 +17,7 @@ class SignUpViewModel {
         password: String,
         about: String
     ) {
-        when (RegexCredentialsValidator().credentialsValidationResult(email, password)) {
+        when (credentialsValidator.credentialsValidationResult(email, password)) {
             is CredentialsValidationResult.InvalidEmail -> {
                 _mutableSignUpState.value = SignUpState.BadEmail
             }
@@ -23,32 +26,6 @@ class SignUpViewModel {
                 _mutableSignUpState.value = SignUpState.BadPassword
             }
         }
-    }
-
-    class RegexCredentialsValidator {
-        fun credentialsValidationResult(
-            email: String,
-            password: String
-        ): CredentialsValidationResult {
-            val EMAIL_REGEX =
-                """[a-zA-Z0-9+._%\-]{1,64}@[a-zA-Z0-9][a-zA-Z0-9\-]{1,64}(\.[a-zA-Z0-9][a-zA-Z0-9\-]{1,25})"""
-            val PASSWORD_REGEX = """^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*+=\-]).{8,}$"""
-
-            val emailPattern = Pattern.compile(EMAIL_REGEX)
-            val passwordPattern = Pattern.compile(PASSWORD_REGEX)
-
-            val result = if (!emailPattern.matcher(email).matches()) {
-                CredentialsValidationResult.InvalidEmail
-            } else if (!passwordPattern.matcher(password).matches()) {
-                CredentialsValidationResult.InvalidPassword
-            } else TODO()
-            return result
-        }
-    }
-
-    sealed class CredentialsValidationResult {
-        object InvalidEmail : CredentialsValidationResult()
-        object InvalidPassword : CredentialsValidationResult()
     }
 
 }
